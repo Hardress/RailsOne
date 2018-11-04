f77ab654869a12cb1b693c143bf884b35b070d07
Hardress <jonsupermurray@gmail.com>
2018-11-04 13:44:53 -0500

    Updated landing page

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,11 @@
 class SimplePagesController < ApplicationController
   def index
+    @products = Product.limit(3)
   end
 end
 
 class SimplePagesController < ApplicationController
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
   end
 end
