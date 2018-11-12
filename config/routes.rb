b94f6612ea0918ebc667ab71ad498562043f6208
Hardress <jonsupermurray@gmail.com>
2018-11-12 11:17:10 -0500

    5.1 Added dynamic links and redirects

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :products, :invoices, :orders, :users # 5.1 added ", :invoices, :orders, :users"
-  resources :orders, only: [:index, :show, :create, :destroy]
+  resources :users, except: [:index]
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  get 'simple_pages/landing_page'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
