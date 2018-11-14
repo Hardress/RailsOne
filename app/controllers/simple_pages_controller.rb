dd780238796df0245ba0f1d920c91bd0820593d0
Hardress <jonsupermurray@gmail.com>
2018-11-14 07:27:05 -0500

    Added Sendgrid to Heroku

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,4 +7,14 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(3)
     @featured_product = Product.first
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'your-email@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
 end
