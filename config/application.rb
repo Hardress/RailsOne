e879998b420c9c4fcaa41a96052fe1708e674d1f
Hardress <jonsupermurray@gmail.com>
2018-12-03 09:47:25 -0500

    6.10 troubleshooting heroku precompile error

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,5 @@ module RailsOne
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
   end
 end
